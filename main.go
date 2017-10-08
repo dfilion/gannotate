8232326a5a6625302e9b9f27169b1fed742f7b01
David Filion <dfilion@filiontech.com>
2017-10-08 11:52:12 -0400

    Updated to new application name.

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func parseInfluxdbTags(tags string) (map[string]string, error) {
 }
 
 func usage(exitCode int) {
-	fmt.Println(`Usage of write-annotate:
+	fmt.Println(`Usage of gannotate:
 	-D string	InfluxDB database name. Default: annotations
 	-H string	InfluxDB server URL. Default: http://localhost:8086
 	-T string	Comma separated list of key=value InfluxDB tags.
